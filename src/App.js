10e4b180648812f20a25fe98ccd352f70ae25dd6
Darshkumar Jasani <jasanidarsh3@gmail.com>
2023-03-15 11:48:15 -0700

    File Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import './App.css'
 const App = ()=>{
 
   const [details,setDetails] = useState({
@@ -9,55 +9,97 @@ const App = ()=>{
     phone_number:''
   })
 
-  const callName = (event)=>{
+  const inputChange = (event)=>{
+    const {value,name} = event.target;
     setDetails((preValue)=>{
-      return({
-        name:event.target.value,
-        password:preValue.password,
-        email:preValue.email,
-        phone_number:preValue.phone_number
-      });
-    })
-  }
+        return({
+          ...preValue,
+          [name] : value
+        })
 
-  const callPwd = (event)=>{
-    setDetails((preValue)=>{
-      return({
-        name:preValue.name,
-        password:event.target.value,
-        email:preValue.email,
-        phone_number:preValue.phone_number
-      });
+      // if(name === 'name' ){
+      //   return({
+      //     name:event.target.value,
+      //     password:preValue.password,
+      //     email:preValue.email,
+      //     phone_number:preValue.phone_number
+      //   });
+      // }
+      // else if(name === 'pwd'){
+      //   return({
+      //     name:preValue.name,
+      //     password:event.target.value,
+      //     email:preValue.email,
+      //     phone_number:preValue.phone_number
+      //   });
+      // }
+      // else if(name === 'email'){
+      //   return({
+      //     name:preValue.name,
+      //     password:preValue.password,
+      //     email:event.target.value,
+      //     phone_number:preValue.phone_number
+      //   });
+      // }
+      // else if(name === 'phno'){
+      //   return({
+      //     name:preValue.name,
+      //     password:preValue.password,
+      //     email:preValue.email,
+      //     phone_number:event.target.value
+      //   });
+      // }
     })
   }
+  // const callName = (event)=>{
+  //   setDetails((preValue)=>{
+  //     return({
+  //       name:event.target.value,
+  //       password:preValue.password,
+  //       email:preValue.email,
+  //       phone_number:preValue.phone_number
+  //     });
+  //   })
+  // }
 
-  const callEmail = (event)=>{
-    setDetails((preValue)=>{
-      return({
-        name:preValue.name,
-        password:preValue.password,
-        email:event.target.value,
-        phone_number:preValue.phone_number
-      });
-    })
-  }
+  // const callPwd = (event)=>{
+  //   setDetails((preValue)=>{
+  //     return({
+  //       name:preValue.name,
+  //       password:event.target.value,
+  //       email:preValue.email,
+  //       phone_number:preValue.phone_number
+  //     });
+  //   })
+  // }
 
-  const callPhno = (event)=>{
-    setDetails((preValue)=>{
-      return({
-        name:preValue.name,
-        password:preValue.password,
-        email:preValue.email,
-        phone_number:event.target.value
-      });
-    })
-  }
+  // const callEmail = (event)=>{
+  //   setDetails((preValue)=>{
+  //     return({
+  //       name:preValue.name,
+  //       password:preValue.password,
+  //       email:event.target.value,
+  //       phone_number:preValue.phone_number
+  //     });
+  //   })
+  // }
+
+  // const callPhno = (event)=>{
+  //   setDetails((preValue)=>{
+  //     return({
+  //       name:preValue.name,
+  //       password:preValue.password,
+  //       email:preValue.email,
+  //       phone_number:event.target.value
+  //     });
+  //   })
+  // }
 
   
 
   const submitClicked = (event)=>{
     event.preventDefault();
-    console.log(details)
+    alert(details.email);
   }
 
   return(
@@ -68,28 +110,28 @@ const App = ()=>{
           type='text'
           placeholder='Enter your Name'
           name='name'
-          onChange={callName}
+          onChange={inputChange}
         />
         <br/>
         <input
           type='password'
           placeholder='Enter your Password'
           name='pwd'
-          onChange={callPwd}
+          onChange={inputChange}
         />
         <br/>
         <input
           type='email'
           placeholder='Enter your Email'
           name='email'
-          onChange={callEmail}
+          onChange={inputChange}
         />
         <br/>
         <input
           type='number'
           placeholder='Enter your Phone Number'
           name='phno'
-          onChange={callPhno}
+          onChange={inputChange}
         />
         <br/>
         <button type='submit'>Submit</button>
